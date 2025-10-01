643f059d00fd1f2b520fc4b2ff376f92cfe32c9a
ZiddFr <148290169+ZiddFr@users.noreply.github.com>
2025-10-01 06:47:37 -0700

    Update vite.config.js

--- proyectos/ecommerce/Bonanza/vite.config.js
+++ proyectos/ecommerce/Bonanza/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bonanza",
+  base: "/mi-portfolio/proyectos/ecommerce/Bonanza",
   plugins: [react()],
 })
